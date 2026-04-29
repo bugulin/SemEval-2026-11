f9c85ad0c4c4e9c11e8790a94a4b8e9f235f30c2
Jajasek <jachym.mierva@gmail.com>
2026-04-30 00:44:34 +0200

    Fix and improve metacentrum training script

--- scripts/training_job.sh
+++ scripts/training_job.sh
@@ -1,11 +1,36 @@
 #!/bin/bash
 #PBS -N npfl140_training
-#PBS -l select=1:ncpus=1:mem=8gb:scratch_local=1gb
-#PBS -l walltime=2:00:00
+#PBS -l select=1:ncpus=2:ngpus=1:mem=16gb:gpu_mem=24gb:scratch_local=64gb
+#PBS -l walltime=15:00:00
 
-BRANCH=${1:-main}
+usage() {
+    echo "Usage: $0 [--branch BRANCH] [FINE_TUNE_ARGS...]"
+    echo
+    echo "  --branch BRANCH   Branch or commit to checkout (default: main)"
+    echo "  FINE_TUNE_ARGS    Additional arguments passed to 'src/cli.py fine-tune'"
+    exit 1
+}
+
+BRANCH="main"
+FINE_TUNE_ARGS=()
+
+while [[ $# -gt 0 ]]; do
+    case "${1}" in
+        -h|--help)
+            usage
+            ;;
+        --branch)
+            BRANCH="${2}"
+            shift 2
+            ;;
+        *)
+            FINE_TUNE_ARGS+=("${1}")
+            shift
+            ;;
+    esac
+done
+
 REPOSITORY="https://github.com/bugulin/SemEval-2026-11"
-
 outdir="out"
 
 trap 'clean_scratch' EXIT
@@ -14,19 +39,17 @@ cd "${SCRATCHDIR}" || exit
 
 # Prepare the environment
 git clone --branch="${BRANCH}" --depth=1 "${REPOSITORY}" repo
+cd repo || exit
 curl -LsSf https://astral.sh/uv/install.sh | env UV_PRINT_QUIET=1 UV_UNMANAGED_INSTALL="bin" sh
 mkdir -p "${outdir}"
 
 # Print out the basic info about the job
 echo "Hello ${PBS_JOBNAME} at $(date) from user ${USER}!"
 echo "${PBS_JOBID} is running on node $(hostname -f) in a scratch directory ${SCRATCHDIR}"
+echo "Command: $0 ${BRANCH} ${FINE_TUNE_ARGS[*]}"
 
 # Run the main task
-bin/uv run src/cli.py fine-tune -o "${outdir}"
+bin/uv run src/cli.py fine-tune -o "${outdir}" "${FINE_TUNE_ARGS[@]}"
 
 # Clean up
 mv "${outdir}" "${PBS_O_WORKDIR}/${PBS_JOBNAME}.${PBS_JOBID}"
-## Optionally, clean after uv
-# uv cache clean
-# rm -r "$(uv python dir)"
-# rm -r "$(uv tool dir)"
